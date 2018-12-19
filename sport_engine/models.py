a550f89902b7835abdf7a86f4ad0eed7d70c13d1
ML expert <vasile123andronic@gmail.com>
2018-12-19 10:38:42 -0800

    model

--- sport_engine/models.py
+++ sport_engine/models.py
@@ -35,18 +35,25 @@ class SportEngineGame(BaseSportEngineModel):
         return 'sport_engine_id=%i, title=%s' % (self.sport_engine_id, self.data.title)
 
 
-
-
-class SportEngineGame(BaseSportEngineModel):
-    sport_engine_teams = models.ManyToManyField(SportEngineTeam, related_name='sport_engine_games')
-    athletes = models.ManyToManyField('multidb_account.AthleteUser', blank=True, related_name='sport_engine_games')
+class SportEngineEvent(BaseSportEngineModel):
+    sport_engine_team = models.ForeignKey(SportEngineTeam, related_name='sport_engine_events')
+    sport_engine_game = models.ForeignKey(SportEngineGame, related_name='sport_engine_events', null=True, blank=True)
+    athletes = models.ManyToManyField('multidb_account.AthleteUser', blank=True, related_name='sport_engine_events')
 
     class Meta:
-        db_table = 'sport_engine_game'
-        verbose_name_plural = _('games')
+        db_table = 'sport_engine_event'
+        verbose_name_plural = _('events')
 
     def __str__(self):
-        return 'sport_engine_id=%i, title=%s' % (self.sport_engine_id, self.data.title)
+        return self.sport_engine_id
+
+
+
+
+
+
+
+
 
 class SportEngineEvent(BaseSportEngineModel):
     sport_engine_team = models.ForeignKey(SportEngineTeam, related_name='sport_engine_events')
