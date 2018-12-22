2e3ec4f78e5db18d1c26dbbdf6cc243a3068cf0b
ML expert <vasile123andronic@gmail.com>
2018-12-22 07:32:29 -0800

    precompetition

--- multidb_account/help_center/admin.py
+++ multidb_account/help_center/admin.py
@@ -17,19 +17,6 @@ class HelpCenterReportAdmin(MultiDBModelAdmin):
 
 
 
-class HelpCenterReportAdmin(MultiDBModelAdmin):
-    sync_databases = True
-    list_display = ('organization', 'coach_name', 'date', 'date_created', 'details', 'name', 'owner')
-    list_select_related = ['owner']
-
-    def has_add_permission(self, request):
-        return False
-
-    @staticmethod
-    def has_delete_permission(request, obj=None, **kwargs):
-        return False
-
-
 class OrganisationSupportAdmin(MultiDBModelAdmin):
     sync_databases = True
     list_display = ('date_created', 'organisation', 'name', 'email', 'phone_number', 'support_type', 'owner')
--- multidb_account/migrations/0011_precompetition.py
+++ multidb_account/migrations/0011_precompetition.py
@@ -31,3 +31,24 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
+
+
+    operations = [
+        migrations.CreateModel(
+            name='PreCompetition',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=255, verbose_name='title')),
+                ('goal', models.CharField(blank=True, max_length=255, verbose_name='title')),
+                ('date', models.DateField(verbose_name='date')),
+                ('date_created', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+                ('stress', models.PositiveSmallIntegerField(verbose_name='stress')),
+                ('fatigue', models.PositiveSmallIntegerField(verbose_name='fatigue')),
+                ('hydration', models.PositiveSmallIntegerField(verbose_name='hydration')),
+                ('injury', models.PositiveSmallIntegerField(verbose_name='injury')),
+                ('weekly_load', models.PositiveSmallIntegerField(verbose_name='weekly load')),
+                ('athlete', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='multidb_account.AthleteUser', verbose_name='athlete')),
+                ('team', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='multidb_account.Team', verbose_name='team')),
+            ],
+        ),
+    ]
