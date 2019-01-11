7aaa05cd87e11575f7cbd77af4c493b665ea6733
High Sky <vasile123andronic@gmail.com>
2019-01-11 13:52:31 -0800

    view

--- rest_api/help_center/views.py
+++ rest_api/help_center/views.py
@@ -35,3 +35,39 @@ class OrganisationSupportViewSet(CreateModelMixin, GenericViewSet):
     def perform_create(self, serializer):
         obj = serializer.save()
         obj.send_notification_emails()
+
+
+
+
+
+
+
+
+class HelpCenterReportViewSet(CreateModelMixin, GenericViewSet):
+    """
+    A viewset for creating help center reports.
+    """
+    permission_classes = (IsAuthenticated,)
+    serializer_class = HelpCenterReportSerializer
+
+    def get_queryset(self):
+        return HelpCenterReport.objects.using(self.request.user.country).select_related('owner')
+
+    def perform_create(self, serializer):
+        obj = serializer.save()
+        obj.send_notification_emails()
+
+
+class OrganisationSupportViewSet(CreateModelMixin, GenericViewSet):
+    """
+    A viewset for creating organisation support tickets.
+    """
+    permission_classes = [IsOrganisationMember]
+    serializer_class = OrganisationSupportSerializer
+
+    def get_queryset(self):
+        return OrganisationSupport.objects.using(self.request.user.country).select_related('owner')
+
+    def perform_create(self, serializer):
+        obj = serializer.save()
+        obj.send_notification_emails()
