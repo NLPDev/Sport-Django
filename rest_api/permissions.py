0063ae9652f6fb0fe926ca233af02fe3cdf040d6
High Sky <vasile123andronic@gmail.com>
2019-01-11 05:11:23 -0800

    permmision

--- rest_api/permissions.py
+++ rest_api/permissions.py
@@ -48,35 +48,3 @@ class IsAuthenticatedAthlete(permissions.BasePermission):
 
 
 
-
-
-
-class IsOwnerOrDeny(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_permission(self, request, view):
-        return is_authenticated(request.user)
-
-    def has_object_permission(self, request, view, obj):
-        # check if user is owner
-        return obj.id == request.user.id
-
-
-class IsValidDisableCustomersToken(permissions.BasePermission):
-    """
-    Custom permission to only allow requests with a valid request.data['token'].
-    """
-
-    def has_permission(self, request, view):
-        return request.data.get('token') == django_settings.DISABLE_EXPIRED_CUSTOMERS_TOKEN
-
-
-class IsAuthenticatedAthlete(permissions.BasePermission):
-    """
-    Custom permissions to only allow requests from athletes
-    """
-
-    def has_permission(self, request, view):
-        return is_authenticated(request.user) and request.user.is_athlete()
