cb6a260e8ce779fc5c9f1349c1605375b659f34d
sky4goal <sky4goal@gmail.com>
2019-08-16 10:41:43 -0700

    file upload part

--- psr/settings/common.py
+++ psr/settings/common.py
@@ -186,3 +186,37 @@ USE_TZ = True
 HELP_CENTER_FORM_EMAILS = []
 # A list of emails to notify on every organisation-support form submission
 HELP_CENTER_ORG_SUPPORT_FORM_EMAILS = []
+
+
+USE_S3 = os.environ.get('USE_S3', False)
+
+if USE_S3:
+    # aws settings    
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+
+    AWS_S3_ENCRYPTION = True
+    AWS_S3_KMS_ENCRYPTION_KEY_ID = os.getenv('AWS_S3_KMS_ENCRYPTION_KEY_ID')
+
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'ops_api.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'ops_api.storage_backends.PublicMediaStorage'
+    # s3 private media settings
+    PRIVATE_MEDIA_LOCATION = 'private'
+    PRIVATE_FILE_STORAGE = 'ops_api.storage_backends.PrivateMediaStorage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
